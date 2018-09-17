f8612dbe2b037de3e25ceeacc726f931ee91268a
zsigui <zsigui@sina.com>
2018-09-17 10:02:57 +0800

    修正KDJ指标中RSV(N)计算错误

--- src/sg/jackiez/worker/utils/algorithm/KDJ.java
+++ src/sg/jackiez/worker/utils/algorithm/KDJ.java
@@ -26,21 +26,22 @@ public class KDJ {
 		KlineInfo tmp;
 		ArrayList<Double> rsv = new ArrayList<>(klineList.size());
 		for (int i = 0; i < totalCount; i++) {
-			// 重设值
-			highest = -1;
-			lowest = -1;
-			close = -1;
+			tmp = klineList.get(i);
+			highest = tmp.highest;
+			lowest = tmp.lowest;
+			close = tmp.close;
 
 			// 数据量小于周期N，只找全部里面最低和最高价，如果数据量大于N，则找周期N内数据量即可
-			int count = (totalCount - i) < n ? (totalCount - i) : n;
-			for (int j = 0; j < count; j++) {
-				tmp = klineList.get(i + j);
+			int firstInPeriod = i <= n ? 0 : i - n;
+			for (int j = firstInPeriod; j < i; j++) {
+				tmp = klineList.get(j);
 				highest = (highest > tmp.highest ? highest : tmp.highest);
 				lowest = (lowest < tmp.lowest ? lowest : tmp.lowest);
 			}
 
 			// RS(N) = (CN - LN) / (HN - LN) * 100
 			// 当 HN = LN, 则 RS(N) = 0
+			// CN为第N日收盘价；LN为N日内的最低价；HN为N日内的最高价
 			if (lowest != highest) {
 				rs = (close - lowest) / (highest - lowest) * 100;
 			} else {
@@ -52,8 +53,8 @@ public class KDJ {
 	}
 
 	/**
-	 * 根据市场盘面数据计算KDJ指标数据
-	 *
+	 * 根据市场盘面数据计算KDJ指标数据 <br />
+	 * P.S.为了确保计算正确，需要保证K线数据是根据时间从小到大排列
 	 * @param klineList
 	 * @return 参数异常及计算失败返回null，成功返回[K数据，D数据，J数据]
 	 */
