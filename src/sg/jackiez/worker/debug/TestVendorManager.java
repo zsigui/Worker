58ff46e49bc7957750dda40f51a2803fa9faef82
zhuangsigui <zhuangsigui@bigo.sg>
2018-10-13 18:28:34 +0800

    模拟测试部分粗糙版

--- src/sg/jackiez/worker/debug/TestVendorManager.java
+++ src/sg/jackiez/worker/debug/TestVendorManager.java
@@ -1,5 +1,6 @@
 package sg.jackiez.worker.debug;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sg.jackiez.worker.module.ok.OKTypeConfig;
@@ -9,6 +10,7 @@ import sg.jackiez.worker.module.ok.callback.FutureDataChangeCallback;
 import sg.jackiez.worker.module.ok.callback.VendorResultCallback;
 import sg.jackiez.worker.module.ok.handler.AccountDataGrabber;
 import sg.jackiez.worker.module.ok.handler.FutureDataGrabber;
+import sg.jackiez.worker.module.ok.handler.FutureVendor;
 import sg.jackiez.worker.module.ok.manager.AccountManager;
 import sg.jackiez.worker.module.ok.model.DepthInfo;
 import sg.jackiez.worker.module.ok.model.Ticker;
@@ -30,8 +32,14 @@ public class TestVendorManager {
 
     private AccountDataGrabber mAccountDataGrabber;
     private FutureDataGrabber mFutureDataGrabber;
+    private FutureVendor mFutureVendor;
     private TestAccount mTestAccount = new TestAccount();
     private boolean mIsDataChange;
+
+
+    private long mLastTradeTime;
+    private double mLastSignal;
+    private String symbol = OKTypeConfig.SYMBOL_EOS;
 
     private AccountStateChangeCallback mStateChangeCallback = new AccountStateChangeCallback() {
         @Override
@@ -54,8 +62,9 @@ public class TestVendorManager {
             klineInfo.time = System.currentTimeMillis();
             klineInfo.close = (depthInfo.asks.get(depthInfo.asks.size() - 1).get(0) +
                     + depthInfo.bids.get(0).get(0)) / 2;
-            mFutureDataGrabber.getKlineInfoMap().get(OKTypeConfig.KLINE_TYPE_1_MIN).add(klineInfo);
-            handleKlineForSignal(mFutureDataGrabber.getKlineInfoMap().get(OKTypeConfig.KLINE_TYPE_1_MIN));
+            List<KlineInfo> klineInfos = new ArrayList<>(mFutureDataGrabber.getKlineInfoMap().get(OKTypeConfig.KLINE_TYPE_1_MIN));
+            klineInfos.add(klineInfo);
+            handleKlineForSignal(klineInfos);
         }
 
         @Override
@@ -72,7 +81,21 @@ public class TestVendorManager {
     private VendorResultCallback mVendorResultCallback = new VendorResultCallback() {
         @Override
         public void onTradeSuccess() {
+            printProfitList();
+            // 重新拉取账户信息
+            AccountManager.get().setNeedUpdateInfo(true);
 
+            double curEosToUsdt;
+            if (mFutureDataGrabber.getDepthInfo() != null) {
+                DepthInfo depthInfo = mFutureDataGrabber.getDepthInfo();
+                curEosToUsdt = (depthInfo.bids.get(0).get(0) + depthInfo.asks.get(depthInfo.asks.size() - 1).get(0));
+            } else {
+                List<KlineInfo> kinfo = mFutureDataGrabber.getKlineInfoMap().get(OKTypeConfig.KLINE_TYPE_1_MIN);
+                curEosToUsdt = kinfo.get(kinfo.size() - 1).close;
+            }
+            SLogUtil.i(TAG, "执行交易完成：当前执行Eos金额是：$" + curEosToUsdt + ", signal = " + mLastSignal);
+            mLastTradeTime = System.currentTimeMillis();
+            handleBuyAndSell(curEosToUsdt, mLastSignal);
         }
 
         @Override
@@ -109,9 +132,16 @@ public class TestVendorManager {
             mIsDataChange = true;
             return;
         }
-        double signal = mPIZ.calculate(klineInfos);
+        if (mLastSignal == Double.MAX_EXPONENT) {
+            return;
+        }
+        if (System.currentTimeMillis() - mLastTradeTime <= 3_000) {
+            return;
+        }
 
+        mIsDataChange = false;
         double curEosToUsdt;
+        double signal = mPIZ.calculate(klineInfos);
         if (mFutureDataGrabber.getDepthInfo() != null) {
             DepthInfo depthInfo = mFutureDataGrabber.getDepthInfo();
             curEosToUsdt = (depthInfo.bids.get(0).get(0) + depthInfo.asks.get(depthInfo.asks.size() - 1).get(0));
@@ -119,6 +149,45 @@ public class TestVendorManager {
             List<KlineInfo> kinfo = mFutureDataGrabber.getKlineInfoMap().get(OKTypeConfig.KLINE_TYPE_1_MIN);
             curEosToUsdt = kinfo.get(kinfo.size() - 1).close;
         }
+        SLogUtil.i(TAG, "当前执行Eos金额是：$" + curEosToUsdt + ", signal = " + signal);
+        mLastSignal = signal;
+        // 随意执行买卖操作
+        if (mTestAccount.isHoldContracts()) {
+            // 没有持有合约，则当前根据信号量执行买入卖出
+            if (signal >= 1) {
+                mFutureVendor.buyLongDirectly(symbol, 10);
+            } else if (signal <= -1) {
+                mFutureVendor.buyLongDirectly(symbol, 10);
+            } else if (signal > 0) {
+                mFutureVendor.buyLongDirectly(symbol, 10);
+            } else if (signal < 0) {
+                mFutureVendor.buyLongDirectly(symbol, 10);
+            }
+        } else {
+            // 持有合约，这个时候判断卖出
+            double profitRate;
+            if (mTestAccount.getHoldDownPage() > 0) {
+                profitRate = 1 - curEosToUsdt / mTestAccount.getHoldDownPageValue() * mTestAccount.getLeverRate();
+                if (-profitRate > TestAccount.MAX_LOSS_RATE) {
+                    // 赔过额度，卖出
+                    mFutureVendor.buyLongDirectly(symbol, 10);
+                } else if (signal > 0 || profitRate > 0.5f) {
+                    mFutureVendor.buyLongDirectly(symbol, 10);
+                }
+            }
+            if (mTestAccount.getHoldUpPage() > 0) {
+                profitRate = 1 - curEosToUsdt / mTestAccount.getHoldUpPageValue() * mTestAccount.getLeverRate();
+                if (-profitRate > TestAccount.MAX_LOSS_RATE) {
+                    mFutureVendor.buyLongDirectly(symbol, 10);
+                } else if (signal < 0 || profitRate > 0.5f) {
+                    mFutureVendor.buyLongDirectly(symbol, 10);
+                }
+            }
+        }
+    }
+
+    private void handleBuyAndSell(double curEosToUsdt, double signal) {
+        mLastSignal = Double.MAX_EXPONENT;
         if (mTestAccount.isHoldContracts()) {
             // 没有持有合约，则当前根据信号量执行买入卖出
             if (signal >= 1) {
@@ -155,13 +224,14 @@ public class TestVendorManager {
 
     public void start() {
         IFutureRestApi futureRestApi = new FutureRestApiV1();
-        String symbol = OKTypeConfig.SYMBOL_EOS;
         String contractType = OKTypeConfig.CONTRACT_TYPE_QUARTER;
         mAccountDataGrabber = new AccountDataGrabber(symbol, contractType,
                 futureRestApi);
         mFutureDataGrabber = new FutureDataGrabber(symbol, contractType, futureRestApi);
         mAccountDataGrabber.startGrabAccountDataThread();
         mFutureDataGrabber.startAll();
+        mFutureVendor = new FutureVendor(futureRestApi, contractType, OKTypeConfig.LEVER_RATE_20);
+        mFutureVendor.startTradeThread();
         CallbackManager.get().addAccountStateChangeCallback(mStateChangeCallback);
         CallbackManager.get().addFutureDataChangeCallback(mDataChangeCallback);
         CallbackManager.get().addVendorResultCallback(mVendorResultCallback);
@@ -170,6 +240,7 @@ public class TestVendorManager {
     public void stop() {
         mAccountDataGrabber.stopGrabAccountDataThread();
         mFutureDataGrabber.stopAll();
+        mFutureVendor.stopTradeThread();
         CallbackManager.get().removeAccountStateChangeCallback(mStateChangeCallback);
         CallbackManager.get().removeFutureDataChangeCallback(mDataChangeCallback);
         CallbackManager.get().removeVendorResultCallback(mVendorResultCallback);
